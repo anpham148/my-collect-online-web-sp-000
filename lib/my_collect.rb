40de0f36d01d2b050e955e175255e752165fa20e
An Pham <anpham148@gmail.com>
2020-06-22 17:46:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,3 +7,12 @@ def my_collect(collection)
     collection
 
 end
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
