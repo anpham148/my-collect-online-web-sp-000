740b33c91b1b51d27966821bd1657dfc60738bd4
An Pham <anpham148@gmail.com>
2020-06-22 17:39:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,16 @@
 def my_collect(collection)
   i = 0
-  while i < collection.length
-    yield(collection[i])
-    i += 1
+
+  if block_given?
+    i = 0
+ 
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+    collection
+  else
+    puts "Hey! No block was given!"
   end
-  collection
+end
 end
